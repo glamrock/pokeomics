e48d7182f64ccbc049febc3eeb57b9eb5645c397
Daniel Assange <somnidea@lemma.org>
2010-10-01 05:54:33 +1000

    Various little addons to pokeswarm!

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,6 +1,19 @@
 class ExperimentsController < ApplicationController
 
-  def parade
-    @overworlds = Pokedex::Pokemon.gen(4).map { |species| species.overworlds }
+  def pokeswarm
+    @data = {}
+
+    spid = Pokedex::Stat.find_by_name('Speed').id
+    @data['baseSpeeds'] = Pokedex::PokemonStat.where(:stat_id => spid).map {|pstat| pstat.base_stat}
+    @data['weights'] = Pokedex::Pokemon.all.map {|species| species.weight}
+    @data['heights'] = Pokedex::Pokemon.all.map {|species| species.height}
+
+    @settings = {
+      :speedStatBias => true,
+      :speedWeightBias => false,
+      :bobbleWeightBias => true,
+      #:sizeHeightBias => true,
+      :order => 'random',
+    }
   end
 end
